709073db694b6a90f7a24a6c439c8ce97a430c77
TimotheSymbolIt <timothe.gauquelin@symbol-it.fr>
2024-07-16 10:14:06 +0200

    add findById
    post a author
    
    delete an author

--- routes/AuthorRoute.js
+++ routes/AuthorRoute.js
@@ -6,4 +6,16 @@ router.get("/findAll", (request, response) =>
 	AuthorController.getAllAuthor(request, response)
 );
 
+router.get("/findById", (request, response) =>
+	AuthorController.getAllAuthor(request, response)
+);
+
+router.post("/", (request, response) =>
+	AuthorController.getAllAuthor(request, response)
+);
+
+router.delete("/", (request, response) =>
+	AuthorController.getAllAuthor(request, response)
+);
+
 module.exports = router;
